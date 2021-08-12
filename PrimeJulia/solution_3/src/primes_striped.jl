2b4f3be721c1d829beb69c24f6620a4094d319e9
Louie Torres <louie.btowers@gmail.com>
2021-08-12 19:33:13 +0800

    Start implementing extra_bits

--- PrimeJulia/solution_3/src/primes_striped.jl
+++ PrimeJulia/solution_3/src/primes_striped.jl
@@ -6,41 +6,49 @@ export PrimeSieveStripedBlocks
 
 import ..PrimesSolution3
 
-const MainUInt = UInt8
+const BlockUInt = UInt8
+const ExtraBitsUInt = UInt32
 # If MainUInt is longer than Int, the bit mask calculation in
 # clear_stripe (1 << bit_idx) won't work.
 # For some reason, 1 << bit_idx is faster than MainUInt(1) << bit_idx.
-if sizeof(MainUInt) > sizeof(Int)
-    error("MainUInt must not be larger than native UInt type.")
+if sizeof(BlockUInt) > sizeof(Int)
+    error("BlockUInt must not be larger than native UInt type.")
 end
-const UINT_BIT_LENGTH = sizeof(MainUInt) * 8
+const BLOCKUINT_BIT_LENGTH = sizeof(BlockUInt) * 8
+const EXTRABITSUINT_BIT_LENGTH = sizeof(ExtraBitsUInt) * 8 
 const BLOCK_SIZE = 16 * 1024
-const BLOCK_SIZE_BITS = BLOCK_SIZE * UINT_BIT_LENGTH
+const BLOCK_SIZE_BITS = BLOCK_SIZE * BLOCKUINT_BIT_LENGTH
 
 @inline _mul2(i::Integer) = i << 1
 @inline _div2(i::Integer) = i >> 1
 @inline _map_to_index(i::Integer) = _div2(i - 1)
 @inline _map_to_factor(i::Integer) = _mul2(i) + 1
-# This is more accurate than _div_uint_size(_div2(i)) + 1
-# We only store odd numbers starting from 3, so we subtract 2 from i
-# to account for that.
-@inline function _get_num_blocks(i::Integer)
-    return (i - 2 + (2 * BLOCK_SIZE_BITS - 1)) ÷ 2 ÷ BLOCK_SIZE_BITS
-end
-
 
 struct PrimeSieveStripedBlocks <: PrimesSolution3.AbstractPrimeSieve
     sieve_size::UInt
-    length_bits::UInt
-    blocks::Vector{Vector{MainUInt}}
+    total_bits::UInt
+    blocks::Vector{Vector{BlockUInt}}
+    total_blocks_bits::UInt
+    extra_bits::Vector{ExtraBitsUInt}
+    total_extra_bits::UInt
 end
 
 
 function PrimeSieveStripedBlocks(sieve_size::UInt)
+    sieve_size ÷= 2
+    num_blocks = sieve_size ÷ BLOCK_SIZE_BITS
+    total_bits_in_blocks = sieve_size * BLOCK_SIZE_BITS
+    num_extra_bits = sieve_size % BLOCK_SIZE_BITS
     return PrimeSieveStripedBlocks(
         sieve_size,
         _map_to_index(sieve_size),
-        [fill(typemax(MainUInt), BLOCK_SIZE) for _ in 1:_get_num_blocks(sieve_size)]
+        [fill(typemax(BlockUInt), BLOCK_SIZE) for _ in 1:num_blocks],
+        total_bits_in_blocks,
+        zeros(
+            ExtraBitsUInt,
+            (num_extra_bits + EXTRABITSUINT_BIT_LENGTH - 1) ÷ EXTRABITSUINT_BIT_LENGTH
+        ),
+        num_extra_bits
     )
 end
 
@@ -51,15 +59,22 @@ PrimeSieveStripedBlocks(sieve_size::Int) = PrimeSieveStripedBlocks(UInt(sieve_si
 @inline function unsafe_get_bit_at_zero_index(
     sieve::PrimeSieveStripedBlocks, index::Integer
 )
-    if index >= sieve.length_bits
+    if index >= sieve.total_bits
         return false
+    elseif index >= sieve.total_blocks_bits
+        index -= sieve.total_blocks_bits
+        return iszero(
+            sieve.extra_bits[index ÷ EXTRABITSUINT_BIT_LENGTH + 1] & 
+            (1 << (index % EXTRABITSUINT_BIT_LENGTH))
+        )
+    else
+        block_index = index ÷ BLOCK_SIZE_BITS
+        offset = index % BLOCK_SIZE_BITS
+        bit_index = offset ÷ BLOCK_SIZE
+        word_index = offset % BLOCK_SIZE
+        word = @inbounds sieve.blocks[block_index + 1][word_index + 1]
+        return !iszero(word & (1 << bit_index))
     end
-    block_index = index ÷ BLOCK_SIZE_BITS
-    offset = index % BLOCK_SIZE_BITS
-    bit_index = offset ÷ BLOCK_SIZE
-    word_index = offset % BLOCK_SIZE
-    word = @inbounds sieve.blocks[block_index + 1][word_index + 1]
-    return !iszero(word & (1 << bit_index))
 end
 
 @inline unsafe_get_bit_at_index(
@@ -88,7 +103,7 @@ end
 end
 
 Base.@propagate_inbounds function clear_stripe!(
-    block::Vector{MainUInt},
+    block::Vector{BlockUInt},
     word_idx::Integer,
     skip::Integer,
     mask::Integer,
@@ -113,17 +128,34 @@ Base.@propagate_inbounds function clear_stripe!(
     return word_idx
 end
 
+Base.@propagate_inbounds function clear_extra_bits!(
+    sieve::PrimeSieveStripedBlocks,
+    start::Integer,
+    skip::Integer,
+)
+    i = start
+    while i < sieve.total_extra_bits
+        sieve.extra_bits[i ÷ EXTRABITSUINT_BIT_LENGTH + 1] |= 1 << (i % EXTRABITSUINT_BIT_LENGTH)
+        i += skip
+    end
+end
+
 function PrimesSolution3.unsafe_clear_factors!(
     sieve::PrimeSieveStripedBlocks,
     factor_index::Integer
 )
     factor = _map_to_factor(factor_index)
-    start = factor * factor ÷ 2 - 1
+    start = factor * factor ÷ 2
     skip = factor
+    if start >= sieve.total_blocks_bits
+        start -= sieve.total_blocks_bits
+        @inbounds clear_extra_bits!(sieve, start, skip)
+        return
+    end
 
     blocks = sieve.blocks
     num_blocks = length(blocks)
-    length_bits = sieve.length_bits
+    length_bits = sieve.total_bits
 
     block_index = start ÷ BLOCK_SIZE_BITS
     offset_index = start % BLOCK_SIZE_BITS
@@ -132,19 +164,9 @@ function PrimesSolution3.unsafe_clear_factors!(
 
     @inbounds while block_index < num_blocks
         block = blocks[block_index + 1]
-        while bit_index < UINT_BIT_LENGTH
-            # Calculate length of current stripe, if it is less than
-            # BLOCK_SIZE, then we know that this should be the last
-            # iteration.
-            stripe_start_position = block_index * BLOCK_SIZE_BITS + bit_index * BLOCK_SIZE
-            effective_length = length_bits - stripe_start_position
+        while bit_index < BLOCKUINT_BIT_LENGTH
             mask = ~(1 << bit_index)
-            if effective_length < BLOCK_SIZE
-                clear_stripe!(block, word_index, skip, mask, effective_length)
-                return
-            else
-                word_index = clear_stripe!(block, word_index, skip, mask, BLOCK_SIZE)
-            end
+            word_index = clear_stripe!(block, word_index, skip, mask, BLOCK_SIZE)
             bit_index += 1
             word_index -= BLOCK_SIZE
         end
@@ -152,6 +174,10 @@ function PrimesSolution3.unsafe_clear_factors!(
         bit_index = zero(bit_index)
         block_index += 1
     end
+    
+    index = block_index * BLOCK_SIZE_BITS + bit_index * BLOCK_SIZE + word_index
+    index -= sieve.total_blocks_bits
+    clear_extra_bits!(sieve, index, skip)
 end
 
 function PrimesSolution3.run_sieve!(sieve::PrimeSieveStripedBlocks)
@@ -175,7 +201,7 @@ function PrimesSolution3.count_primes(sieve::PrimeSieveStripedBlocks)
     # Since we start clearing factors at 3, we include 2 in the count
     # beforehand.
     count = 1
-    for i in 1:sieve.length_bits
+    for i in 1:sieve.total_bits
         if unsafe_get_bit_at_index(sieve, i)
             count += 1
         end
@@ -187,7 +213,7 @@ function PrimesSolution3.get_found_primes(sieve::PrimeSieveStripedBlocks)
     output = [2]
     # Int(sieve_size) may overflow if sieve_size is too large (most
     # likely only a problem for 32-bit systems)
-    for (index, number) in zip(1:sieve.length_bits, 3:2:Int(sieve.sieve_size))
+    for (index, number) in zip(1:sieve.total_bits, 3:2:Int(sieve.sieve_size))
         if unsafe_get_bit_at_index(sieve, index)
             push!(output, number)
         end
